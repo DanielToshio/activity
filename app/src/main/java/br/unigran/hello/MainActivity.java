75ee90c04348b9672597773db6bc0fcb1bdcbfa5
DanielToshio <802.261@alunos.unigran.br>
2022-10-05 21:48:17 -0300

    activity

---
+++ app/src/main/java/br/unigran/hello/MainActivity.java
@@ -0,0 +1,41 @@
+package br.unigran.hello;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResultRegistry;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void next(View view){
+        Intent it = new Intent(this,SegundaActivity.class);
+        it.putExtra("Nome","André");
+        resultLauncher.launch(it);
+    }
+    ActivityResultLauncher resultLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>(){
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                           if(result.getResultCode()==80)
+                            Toast.makeText(getApplicationContext(),
+                                    "retorno",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+}
